2b37e1f50a88dfa9c9815d682e5ef373ac291b93
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-13 13:30:25 +0400

    add operations

---
+++ main.py
@@ -0,0 +1,11 @@
+def add(n1,n2):
+
+    operations = {
+        "+" : add,
+        "-" : subtract,
+        "*" : multiply,
+        "/" : divide 
+    }
+
+    number1 = int(input("What is the first number?: "))
+    number2 = int(input("What is the second number?: "))
