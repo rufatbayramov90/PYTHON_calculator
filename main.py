1c669d18065954313f4a34386f45a8e4554a52a5
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-13 13:40:54 +0400

    for loop for symbols

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 def add(n1,n2):
+    return n1 +n2
+def subtract(n1,n2):
+    return n1 - n2
+def multiply(n1,n2):
+    return n1 * n2
+def divide(n1,n2):
+    return n1 / n2
 
-    operations = {
-        "+" : add,
-        "-" : subtract,
-        "*" : multiply,
-        "/" : divide 
-    }
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide 
+}
 
-    number1 = int(input("What is the first number?: "))
-    number2 = int(input("What is the second number?: "))
+number1 = int(input("What is the first number?: "))
+number2 = int(input("What is the second number?: "))
+
+
+for symbol in operations:
+    print(symbol)
+symbol_Operations = input("Pick an operation from the line above")
