1f975a6f3129ede6beb77087834a7962f0a5a637
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-13 14:39:36 +0400

    Reusing the received answer

--- main.py
+++ main.py
@@ -22,7 +22,13 @@ for symbol in operations:
     print(symbol)
 symbol_Operations = input("Pick an operation from the line above")
 calculation_function = operations[symbol_Operations]
-answer = calculation_function(number1,number2)
+answer_first = calculation_function(number1,number2)
 
-print(f"{number1} {symbol_Operations} {number2} = {answer}")
+print(f"{number1} {symbol_Operations} {number2} = {answer_first}")
 
+
+symbol_Operations = input("Pick another operation: ")
+number3 = int(input("What is the next number? : "))
+calculation_function = operations[symbol_Operations]
+answer_second = calculation_function(calculation_function(number1,number2),number3)
+print(f"{answer_first} {symbol_Operations} {number3} = {answer_second}")
